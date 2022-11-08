145b796881279b45d041fe8332a512ed2d5ca9da
Jason X <59701038+JChunX@users.noreply.github.com>
2022-11-07 23:36:52 -0500

    add xml models

--- quadruped_sim/src/main.cpp
+++ quadruped_sim/src/main.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 #include<filesystem>
-#include"QuadrupedSim.h"
+//#include"QuadrupedSim.h"
 
-int main(int argc, char** argv)
-{
-    std::string path = std::filesystem::current_path().string();
-    std::cout << "Current path is : " << path << std::endl;
-    QuadrupedSim sim;
-    sim.run();
+int main(int argc, char** argv) {
+
+    
+
     return 0;
 }
